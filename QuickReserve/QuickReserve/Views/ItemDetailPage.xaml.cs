32767485c9b7c5498ba122788117f214ab06681f
ZoldiTamasBotond <99806577+ZoldiTamasBotond@users.noreply.github.com>
2024-10-14 20:23:09 +0300

    Add project files.

---
+++ QuickReserve/QuickReserve/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QuickReserve.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QuickReserve.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
