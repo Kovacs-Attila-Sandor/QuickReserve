8519a396c134f4e135561a2ea49cc4211ad9ad46
ZoldiTamasBotond <99806577+ZoldiTamasBotond@users.noreply.github.com>
2024-11-12 15:40:49 +0200

    database connection waiting in registration

--- QuickReserve/QuickReserve/Views/UserRegisterPage.xaml.cs
+++ QuickReserve/QuickReserve/Views/UserRegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickReserve.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,32 @@ namespace QuickReserve.Views
         protected void GoToLoginPage(object sender, EventArgs e)
         {
             App.Current.MainPage = new NavigationPage(new LoginPage());
+        }
+
+        private async void Regiser(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUsername.Text) && !string.IsNullOrEmpty(txtPasswod.Text) && !string.IsNullOrEmpty(txtConfirmPasswod.Text) && !string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPhonenum.Text))
+            {
+                //if (await App.SQLiteDb.GetEventsAsync(txtUsername.Text) == null)    //FIREBASE DATABASE
+                //{
+                    if (string.Equals(txtPasswod.Text, txtConfirmPasswod))
+                    {
+                        User user = new User()
+                        {
+                            Name = txtUsername.Text,
+                            Password = txtPasswod.Text,
+                            Email = txtEmail.Text,
+                            PhoneNumber = txtPhonenum.Text,
+                            Role = "USER"
+                        };
+                        //await App.SQLiteDb.SaveItemAsync(events);
+                    }
+                    else await DisplayAlert("SAVING ERROR", "Your password is incorrect", "OK");
+
+                // }
+                // else await DisplayAlert("SAVING ERROR", "This Username is already used", "OK");
+            }
+            else await DisplayAlert("SAVING ERROR", "Something is empty", "OK");
         }
     }
 }
