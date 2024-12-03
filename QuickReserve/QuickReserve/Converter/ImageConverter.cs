3b74da6421d97798b95a3727bac5ee067c3f372b
Kovacs-Attila-Sandor <kovacs.s.attila@student.ms.sapientia.ro>
2024-12-03 18:52:50 +0200

    asd

---
+++ QuickReserve/QuickReserve/Converter/ImageConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+public static class ImageConverter
+{
+    public static string ConvertToBase64(string imagePath)
+    {
+        byte[] imageBytes = File.ReadAllBytes(imagePath);
+        return Convert.ToBase64String(imageBytes);
+    }
+}
---
+++ QuickReserve/QuickReserve/Models/RestaurantLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickReserve.Models
+{
+    public class RestaurantLocation
+    {       
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+    }
+}
